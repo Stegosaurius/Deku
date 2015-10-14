392f67db5e1c0032063f422038a6a64fe52d4007
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-14 11:55:54 -0700

    Add skeleton controller, router, and model for user

---
+++ server/users/userController.js
@@ -0,0 +1,6 @@
+//Require our user schema. We will use this in our controller functions
+var User = require('./userModel.js');
+
+module.exports = {
+  //Put all server functions regarding users here.
+}
