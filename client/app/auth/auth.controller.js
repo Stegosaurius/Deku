06426b61a50a9abd3fd3c5faa924f978489c9a9c
John Anderson <jfanderson373@gmail.com>
2015-10-15 12:06:58 -0700

    Initial client-side setup

---
+++ client/app/auth/auth.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .controller('AuthController', AuthController);
+
+  AuthController.$inject('$scope', 'Auth');
+
+  function AuthController($scope, Auth) {
+    // insert code here
+  }
+})();
---
+++ client/app/profile/profile.controller.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .controller('ProfileController', ProfileController);
+
+  ProfileController.$inject(['$scope']);
+
+  function ProfileController($scope) {
+    // insert code here
+  }
+})();
