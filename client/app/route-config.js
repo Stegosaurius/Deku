39730cd870b026ba7c0b532370bc426e72b78d37
John Anderson <jfanderson373@gmail.com>
2015-10-15 20:03:51 -0700

    Setup routing

--- client/app/route-config.js
+++ client/app/route-config.js
@@ -7,8 +7,33 @@
   config.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider'];
 
   function config($stateProvider, $urlRouterProvider, $httpProvider) {
+    // default path
+    $urlRouterProvider.otherwise('/dashboard');
 
-    // set up routes
-
+    $stateProvider
+      .state('signin', {
+        url: '/signin',
+        templateUrl: 'app/auth/signin.html',
+        controller: 'app/auth/signin.controller.js',
+        controllerAs: 'signin'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'app/auth/signup.html',
+        controller: 'app/auth/signup.controller.js',
+        controllerAs: 'signup'
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'app/dashboard/dashboard.controller.js',
+        controllerAs: 'dashboard'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'app/profile/profile.html',
+        controller: 'app/profile/profile.controller.js',
+        controllerAs: 'profile'
+      });
   }
 })();
