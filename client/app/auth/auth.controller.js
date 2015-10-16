dae44e9b9c9abc07db59613551d58a465a752250
John Anderson <jfanderson373@gmail.com>
2015-10-16 14:46:44 -0700

    Setup sign in/sign up pages and auth controller

--- client/app/auth/auth.controller.js
+++ client/app/auth/auth.controller.js
@@ -4,9 +4,23 @@
   angular.module('app')
     .controller('AuthController', AuthController);
 
-  AuthController.$inject = ['$scope', 'Auth'];
+  AuthController.$inject = ['User'];
 
-  function AuthController($scope, Auth) {
-    // insert code here
+  function AuthController(User) {
+    // capture variable for binding members to controller; vm stands for ViewModel
+    // (https://github.com/johnpapa/angular-styleguide#controlleras-with-vm)
+    var vm = this;
+
+    vm.signin = signin;
+    vm.signup = signup;
+    vm.user = {};
+
+    function signin() {
+      User.signin(vm.user);
+    }
+
+    function signup() {
+      User.signup(vm.user);
+    }
   }
 })();
