af72e19af688b47fb8d6d4abc3d0c4bddcf10e08
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-17 11:36:32 -0700

    Added skeleton for test folder and controller specs

---
+++ test/server/controllerSpec.js
@@ -0,0 +1,92 @@
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+//Require all of our controllers
+var userController = require('/server/controllers/userController.js');
+var threadController = require('/server/controllers/threadController.js');
+var followerController = require('/server/controllers/followerController.js');
+
+//Stub out model methods?
+
+/////////////////////
+/////CONTROLLERS/////
+/////////////////////
+
+
+describe('Server side controller testing', function () {
+
+  /////////////////////
+  ///userController////
+  /////////////////////
+
+  describe('userController requirements', function () {
+
+    it('should have a method called getProfile', function () {
+      
+    });
+
+    it('should have a method called getProfile', function () {
+      ;
+    })
+
+    it('should have a method called getProfile', function () {
+      
+    });
+
+    it('should have a method called getProfile', function () {
+
+    });
+  }
+
+  /////////////////////
+  ///threadController//
+  /////////////////////
+
+  describe('threadController requirements', function () {
+
+    it('should have a method called getProfile', function () {
+      
+    });
+
+    it('should have a method called getProfile', function () {
+      ;
+    })
+
+    it('should have a method called getProfile', function () {
+      
+    });
+
+    it('should have a method called getProfile', function () {
+
+    });
+  }
+
+  ///////////////////////
+  //followerController///
+  ///////////////////////
+  
+  describe('followerController requirements', function () {
+
+    
+
+    it('should have a method called getProfile', function () {
+      
+    });
+
+    it('should have a method called getProfile', function () {
+      ;
+    })
+
+    it('should have a method called getProfile', function () {
+      
+    });
+
+    it('should have a method called getProfile', function () {
+
+    });
+  }
+
+
+
+})
