9c9ccfca885af08e8092ba43ce9172d262d22fc7
Edgar Pabon <edgar.pabon@gmail.com>
2015-10-17 15:32:25 -0700

    Server sends JSON Web Tokens back to client on authentication

--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -1,37 +1,65 @@
 var userController = require('../controllers/userController.js');
-
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var auth = require('../config/auth');
 
 module.exports = function (app, passport) {
   //app === userRouter injected from middlware.js
 
   //Specifying which controller function we wish to call
   //based on the request url
-  app.get('/:id', function (req, res) {
+
+  // protect /api routes with JWT
+  app.use('/api', expressJWT({ secret: auth.secret }));
+
+  app.get('/api/users/:id', function (req, res) {
     //Get the id
     var id = req.params.id;
     userController.getProfile(req, res, id);
   });
-  app.put('/:id', function (req, res) {
+
+  app.put('/api/users/:id', function (req, res) {
     //Get the id
     var id = req.params.id;
     userController.updateProfile(req, res, id);
   });
 
-  app.get('/profile', function (req, res) {
-    userController.getUserProfile(req, res);
+
+  app.post('/auth/signup', function (req, res, next) {
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local-signup', function(err, user, info){
+      if (err) { 
+        return next(err); 
+      }
+
+      if (user) {
+        return res.json({ token: util.generateWebToken(user) });
+      } else {
+        return res.status(401).json(info);
+      }
+    })(req, res, next);
   });
 
-  app.post('/auth/signup', passport.authenticate('local-signup', {
-    successRedirect: '/#/profile',
-    failureRedirect: '/#/signup',
-    failureFlash: true
-  }));
+  app.post('/auth/signin', function (req, res, next) {
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).json({message: 'Please fill out all fields'});
+    }
 
-  app.post('/auth/signin', passport.authenticate('local-login', {
-    successRedirect: '/#/dashboard',
-    failureRedirect: '/#/signin',
-    failureFlash: true
-  }));
+    passport.authenticate('local-signin', function(err, user, info){
+      if (err) { 
+        return next(err); 
+      }
+
+      if (user) {
+        return res.json({ token: util.generateWebToken(user) });
+      } else {
+        return res.status(401).json(info);
+      }
+    })(req, res, next);
+  });
 
   // route for logging out
   app.get('/logout', function(req, res) {
@@ -46,10 +74,19 @@ module.exports = function (app, passport) {
   app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
 
   // handle the callback after facebook has authenticated the user
-  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-    successRedirect : '/#/profile',
-    failureRedirect : '/#/signin'
-  }));
+  app.get('/auth/facebook/callback', function (req, res, next) {
+    passport.authenticate('facebook', function (err, user, info) {
+      if (err) { 
+        return next(err); 
+      }
+
+      if (user) {
+        return res.json({ token: util.generateWebToken(user) });
+      } else {
+        return res.status(401).json(info);
+      }
+    })(req, res, next);
+  });
 
   // =====================================
   // GOOGLE ROUTES =======================
@@ -60,9 +97,17 @@ module.exports = function (app, passport) {
   app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
   // the callback after google has authenticated the user
-  app.get('/auth/google/callback', passport.authenticate('google', {
-    successRedirect : '/#/profile',
-    failureRedirect : '/#/signin'
-  }));
+  app.get('/auth/google/callback', function (req, res, next) {
+    passport.authenticate('google', function (err, user, info) {
+      if (err) { 
+        return next(err); 
+      }
+      if (user) {
+        return res.json({ token: util.generateWebToken(user) });
+      } else {
+        return res.status(401).json(info);
+      }
+    })(req, res, next);
+  });
   
 }
