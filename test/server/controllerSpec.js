6acc73bc9bfb5e8f6b8d458ec81d0919acb691e6
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-17 12:02:33 -0700

    Implement basic unit test structure for server controllers

--- test/server/controllerSpec.js
+++ test/server/controllerSpec.js
@@ -1,11 +1,9 @@
-var assert = chai.assert;
-var should = chai.should();
-var expect = chai.expect;
+var expect = require('chai').expect;
 
 //Require all of our controllers
-var userController = require('/server/controllers/userController.js');
-var threadController = require('/server/controllers/threadController.js');
-var followerController = require('/server/controllers/followerController.js');
+var userController = require('../../server/controllers/userController.js');
+var threadController = require('../../server/controllers/threadController.js');
+var followerController = require('../../server/controllers/followerController.js');
 
 //Stub out model methods?
 
@@ -23,21 +21,49 @@ describe('Server side controller testing', function () {
   describe('userController requirements', function () {
 
     it('should have a method called getProfile', function () {
-      
+      expect(userController.getProfile).to.be.a('function');
     });
 
-    it('should have a method called getProfile', function () {
-      ;
-    })
-
-    it('should have a method called getProfile', function () {
-      
+    it('should have a method called updateProfile', function () {
+      expect(userController.updateProfile).to.be.a('function');
     });
 
-    it('should have a method called getProfile', function () {
-
+    it('should have a method called getUser', function () {
+      expect(userController.getUser).to.be.a('function');
     });
-  }
+
+    it('should have a method called getProfilePhoto', function () {
+      expect(userController.getProfilePhoto).to.be.a('function');
+    });
+
+    it('should have a method called addProfilePhoto', function () {
+      expect(userController.addProfilePhoto).to.be.a('function');
+    });
+
+    it('should have a method called getFollowers', function () {
+      expect(userController.getFollowers).to.be.a('function');
+    });
+
+    it('should have a method called addFollower', function () {
+      expect(userController.addFollower).to.be.a('function');
+    });
+
+    it('should have a method called getStatuses', function () {
+      expect(userController.getStatuses).to.be.a('function');
+    });
+
+    it('should have a method called updateStatus', function () {
+      expect(userController.updateStatus).to.be.a('function');
+    });
+
+    it('should have a method called getNotifications', function () {
+      expect(userController.getNotifications).to.be.a('function');
+    });
+
+    it('should have a method called createNotification', function () {
+      expect(userController.createNotification).to.be.a('function');
+    });
+  });
 
   /////////////////////
   ///threadController//
@@ -45,48 +71,44 @@ describe('Server side controller testing', function () {
 
   describe('threadController requirements', function () {
 
-    it('should have a method called getProfile', function () {
-      
+    it('should have a method called getAllThreads', function () {
+      expect(threadController.getAllThreads).to.be.a('function');
     });
 
-    it('should have a method called getProfile', function () {
-      ;
-    })
-
-    it('should have a method called getProfile', function () {
-      
+    it('should have a method called getThread', function () {
+      expect(threadController.getThread).to.be.a('function');
     });
 
-    it('should have a method called getProfile', function () {
-
+    it('should have a method called postToThread', function () {
+      expect(threadController.postToThread).to.be.a('function');
     });
-  }
+
+  });
 
   ///////////////////////
   //followerController///
   ///////////////////////
-  
+
   describe('followerController requirements', function () {
 
     
 
-    it('should have a method called getProfile', function () {
+    xit('should have a method called getProfile', function () {
       
     });
 
-    it('should have a method called getProfile', function () {
-      ;
-    })
-
-    it('should have a method called getProfile', function () {
+    xit('should have a method called getProfile', function () {
       
     });
 
-    it('should have a method called getProfile', function () {
+    xit('should have a method called getProfile', function () {
+      
+    });
+
+    xit('should have a method called getProfile', function () {
 
     });
-  }
+  });
 
+});
 
-
-})
