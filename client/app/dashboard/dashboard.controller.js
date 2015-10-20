038ff49fda9304479e0daaedebf5ca56c9f76d6c
Barry Blaha <barryblaha@gmail.com>
2015-10-19 20:27:50 -0700

    All knobs working. Trigger plots working.

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -7,16 +7,46 @@
   function DashboardController(User,Keenio) {
     var vm = this;
 
-    vm.greeting = "Hi!!!!";
-
-    Keenio.query( function(err,res){
+    Keenio.tempQuery( function(err,res){
       if(err) {
-        console.log('error in keenio.query callback',err);
+        console.log('error in keenio.tempQuery callback',err);
       } else {
         //res is the result of a keen.io query made in keenio.service
         $("#chart-01").val(res).trigger('change');
       }
     });
+
+    Keenio.humidityQuery( function(err,res){
+      if(err) {
+        console.log('error in keenio.humidityQuery callback',err);
+      } else {
+        $("#chart-02").val(res).trigger('change');
+      }
+    });
+
+    Keenio.lightQuery( function(err,res){
+      if(err) {
+        console.log('error in keenio.lightQuery callback',err);
+      } else {
+        $("#chart-03").val(res).trigger('change');
+      }
+    });
+
+    Keenio.soundQuery( function(err,res){
+      if(err) {
+        console.log('error in keenio.soundQuery callback',err);
+      } else {
+        $("#chart-04").val(res).trigger('change');
+      }
+    });
+
+    Keenio.lightTriggerQuery();
+    Keenio.soundTriggerQuery();
+    Keenio.tempTimelineQuery();
+    Keenio.humidityTimelineQuery();
+    Keenio.soundTimelineQuery();
+    Keenio.lightTimelineQuery();
+
 
   }
 })();
--- client/app/services/keenio.service.js
+++ client/app/services/keenio.service.js
@@ -5,21 +5,21 @@
   function Keenio($http) {
     var client = new Keen( dashboardConfigure ); //loading keys in this file
     var services = {
-      query: query // FAKE
+      tempQuery: tempQuery, 
+      humidityQuery : humidityQuery,
+      lightQuery : lightQuery,
+      soundQuery : soundQuery,
+      lightTriggerQuery : lightTriggerQuery,
+      soundTriggerQuery : soundTriggerQuery 
     };
 
     return services;
 
-    // place functions below
-    // FAKE
-    function query(callback) {
+    function tempQuery(callback) {
       Keen.ready(
         function(){
-          // ----------------------------------------
-          //  Temperature Knob 
-          // ----------------------------------------
 
-          //create query here
+          //Temperature Knob Query
           var temperature = new Keen.Query("average", {
             eventCollection: "climate",
             targetProperty: "temp",
@@ -30,7 +30,6 @@
             timeframe: "today"
           });
 
-          //make a knob for the input box
           $("#chart-01").knob({
             'angleArc':250,
             'angleOffset':-125,
@@ -39,10 +38,8 @@
             'min':0,
             'max':120,
             'fgColor': Keen.Dataviz.defaults.colors[1]
-            // 'fgColor': rgb(254, 102, 114)
           });
 
-          //execute the query and pass the result to the callback
           client.run(temperature, function(err, res){
             if(err){
               callback(err);
@@ -54,5 +51,171 @@
         }
       );
     }
+
+    function humidityQuery(callback) {
+      Keen.ready(
+        function(){
+
+          //Humidity Knob Query
+          var humidity = new Keen.Query("average", {
+            eventCollection: "climate",
+            targetProperty: "humidity",
+            // timeframe: {
+            //   start: "2014-10-06T00:00:00.000",
+            //   end: "2014-10-07T00:00:00.000"
+            // }
+            timeframe: "today"
+          });
+
+          $("#chart-02").knob({
+            'angleArc':250,
+            'angleOffset':-125,
+            'readOnly':true,
+            'min':0,
+            'max':50,
+            'fgColor': Keen.Dataviz.defaults.colors[0]
+          });
+          
+          client.run(humidity, function(err, res){
+            if(err){
+              callback(err);
+            }else{
+              callback(null,res.result);
+            }
+          });
+
+        }
+      );
+    }
+
+    function lightQuery(callback) {
+      Keen.ready(
+        function(){
+
+          //Light Knob Query
+          var light = new Keen.Query("average", {
+            eventCollection: "climate",
+            targetProperty: "light",
+            // timeframe: {
+            //   start: "2014-10-06T00:00:00.000",
+            //   end: "2014-10-07T00:00:00.000"
+            // }
+            timeframe: "today"
+          });
+
+          $("#chart-03").knob({
+            'angleArc':250,
+            'angleOffset':-125,
+            'readOnly':true,
+            'step':0.01,
+            'min':0,
+            'max':50,
+            'fgColor': Keen.Dataviz.defaults.colors[2]
+           });
+          
+          client.run(light, function(err, res){
+            if(err){
+              callback(err);
+            }else{
+              callback(null,res.result);
+            }
+          });
+
+        }
+      );
+    }
+
+    function soundQuery(callback) {
+      Keen.ready(
+        function(){
+
+          //Humidity Knob Query
+          var sound = new Keen.Query("average", {
+            eventCollection: "climate",
+            targetProperty: "sound",
+            // timeframe: {
+            //   start: "2014-10-06T00:00:00.000",
+            //   end: "2014-10-07T00:00:00.000"
+            // }
+            timeframe: "today"
+          });
+
+          $("#chart-04").knob({
+            'angleArc':250,
+            'angleOffset':-125,
+            'readOnly':true,
+            'step':0.01,
+            'min':0,
+            'max':100,
+            'fgColor': Keen.Dataviz.defaults.colors[3]
+          });
+
+          client.run(sound, function(err, res){
+            if(err){
+              callback(err);
+            }else{
+              callback(null,res.result);
+            }
+          });
+
+        }
+      );
+    }
+
+
+    function lightTriggerQuery(callback) {
+      Keen.ready(
+        function(){
+
+          //ligth trigger Query
+          var light_timeline = new Keen.Query("count_unique", {
+            eventCollection: "climate",
+            targetProperty: "light-trigger",
+            interval:"hourly",
+            // timeframe: {
+            //   start: "2014-10-06T00:00:00.000",
+            //   end: "2014-10-07T00:00:00.000"
+            // }
+            timeframe: "today"
+          });
+
+          client.draw(light_timeline, document.getElementById("chart-05"), {
+            chartType: "linechart",
+            title: " ",
+            height: 250,
+            width: "auto"
+          });
+
+        }
+      );
+    }
+
+    function soundTriggerQuery(callback) {
+      Keen.ready(
+        function(){
+
+          //ligth trigger Query
+          var sound_timeline = new Keen.Query("count_unique", {
+            eventCollection: "climate",
+            targetProperty: "sound-trigger",
+            interval:"hourly",
+            // timeframe: {
+            //   start: "2014-10-06T00:00:00.000",
+            //   end: "2014-10-07T00:00:00.000"
+            // }
+            timeframe: "today"
+          });
+
+          client.draw(sound_timeline, document.getElementById("chart-06"), {
+            chartType: "linechart",
+            title: " ",
+            height: 250,
+            width: "auto"
+          });
+
+        }
+      );
+    }
+
   }
 })();
