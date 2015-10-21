c0770cc4a35b5b8b5d5f3c243a48e2dc353ba82b
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-21 11:12:13 -0700

    Update front end edit profile controller and template

--- client/app/profile/editProfile.controller.js
+++ client/app/profile/editProfile.controller.js
@@ -9,12 +9,31 @@
     // (https://github.com/johnpapa/angular-styleguide#controlleras-with-vm)
     var vm = this;
 
-    vm.about = '';
-    vm.location = '';
-    vm.plants = [];
+    // vm.about = '';
+    // vm.location = '';
+    // vm.photo = '';
+    // vm.plants = [];
+    // vm.username = '';
     vm.message = '';
 
     vm.updateProfile = updateProfile;
+
+    //Invoke get profile to prepopulate our view model with 
+    //existing data for a user. This way the data object will
+    //be complete when we send it to the database.
+    getProfile();
+
+    function getProfile() {
+      User.getProfile()
+        .then(function(data) {
+          console.log(data);
+          vm.about = data.about;
+          vm.location = data.location;
+          vm.plants = data.plants;
+          vm.username = data.username;
+          // getPhoto();
+        });
+    }
 
     function updateProfile() {
       User.updateProfile(vm)
