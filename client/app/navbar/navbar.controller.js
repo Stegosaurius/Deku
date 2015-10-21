a6bc0d5b9839bb0c6999396e4f97a3c0f746922e
John Anderson <jfanderson373@gmail.com>
2015-10-20 19:00:53 -0700

    Implement navbar controller and html files

---
+++ client/app/navbar/navbar.controller.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('app')
+    .controller('NavbarController', NavbarController);
+
+  function NavbarController(User) {
+    // capture variable for binding members to controller; vm stands for ViewModel
+    // (https://github.com/johnpapa/angular-styleguide#controlleras-with-vm)
+    var vm = this;
+
+    vm.notifications = [];
+    // show notification nav icon if there are notifications
+    // vm.showNotifications = vm.notifications.length;
+    vm.signout = signout;
+
+    // on mobile-sized screen, make the nav bar appear on menu icon click
+    angular.element('.button-collapse').sideNav();
+
+    getNotifications();
+
+    function getNotifications() {
+      // User.getNotifications();
+      // vm.showNotifications = vm.notifications.length;
+    }
+
+    function signout() {
+      User.signout();
+    }
+  }
+})();
