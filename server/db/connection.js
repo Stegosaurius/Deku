61394b524242ee7c1d3764f01f3415fd8bfa5fe3
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-22 11:25:16 -0700

    fix db connection conditional statement

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
 
-var dbURL = process.env.DATABASE_URL || 'localhost';
+var dbURL = process.env.DATABASE_URL;
 
-var connection = mysql.createConnection({
-  host: dbURL, // create || condition to set production host
-  user: 'root',
-  database: 'Deku'
-});
+if (dbURL) {
+  var connection = mysql.createConnection(dbURL);
+} else {
+  var connection = mysql.createConnection({
+    host: 'localhost', 
+    user: 'root',
+    database: 'Deku'
+  });
+}
+
+
 
 connection.connect(function(err) {
   if (err) {
