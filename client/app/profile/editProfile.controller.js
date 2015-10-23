f218ce1ae41bd1b8d61a1678fe32aba977091a48
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-22 21:03:56 -0700

    Initial styling for editProfile page and navbar

--- client/app/profile/editProfile.controller.js
+++ client/app/profile/editProfile.controller.js
@@ -6,7 +6,7 @@
 
   EditProfileController.$inject = ['$window', '$state', 'User'];
 
-  function EditProfileController($state, User) {
+  function EditProfileController($window, $state, User) {
     // capture variable for binding members to controller; vm stands for ViewModel
     // (https://github.com/johnpapa/angular-styleguide#controlleras-with-vm)
     var vm = this;
