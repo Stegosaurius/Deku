13b389bb148c8bf1a1361af513567a118ce19e69
Matthew Loftus <Loftus@Matthews-MacBook-Pro.local>
2015-10-23 10:13:26 -0700

    Add additonal functions to editProfile controller

--- client/app/profile/editProfile.controller.js
+++ client/app/profile/editProfile.controller.js
@@ -18,17 +18,22 @@
     vm.location = '';
     vm.tags = [];
     vm.username = $window.localStorage.username;
-    vm.message = '';
+    vm.photos = [];
 
+    //editProfile user actions
     vm.updateProfile = updateProfile;
     vm.addTag = addTag;
+    vm.removeTag = removeTag;
+    vm.removeFollower = removeFollower;
+    vm.updateAvatar = updateAvatar;
 
     //Invoke get profile to prepopulate our view model with 
     //existing data for a user. This way the data object will
     //be complete when we send it to the database.
     getProfile();
+    //getAvatar();
 
-    function getProfile() {
+    function getProfile () {
       User.getProfile(vm.username)
         .then(function(data) {
           vm.about = data.about;
@@ -39,7 +44,7 @@
     }
 
     //Update location and about sections of profile
-    function updateProfile() {
+    function updateProfile () {
       User.updateProfile(vm)
         .then(function(data) {
           $state.transitionTo('profile');
@@ -49,7 +54,8 @@
         });
     }
 
-    function getFollowers() {
+    //Get all of the current user's followers
+    function getFollowers () {
       User.getFollowers(vm.username)
         .then(function (data) {
           vm.followers = data.followers;
@@ -57,8 +63,15 @@
         });
     }
 
+    //Remove a follower when a user clicks the remove
+    //button on a specific followers list item
+    function removeFollower () {
+      //Remove specified follower from the vm.followers
+      //list and send to the database to update
+    }
+
     //Get current profile picture(avatar)
-    function getAvatar() {
+    function getAvatar () {
       User.getAvatar(vm.username)
         .then(function (avatar) {
           vm.avatar = avatar;
@@ -68,17 +81,45 @@
     }
 
     //Uploading a new profile picture
-    function updateAvatar() {
+    function updateAvatar () {
 
     }
 
     //Add tag to the vm tags list and send the new tag to the 
     //server to store the new tag in the database
-    function addTag() {
+    function addTag () {
       vm.tags.push(vm.newTag);
       console.log(vm.tags);
       vm.newTag='';
       //Invoke updateTags function
+    }
+
+    //Remove a tag when user clicks x on a particular tag item
+    function removeTag () {
+      //remove a tag from the vm.tags array and send either specified
+      //tag or the new tags array to the server to update
+    }
+
+    //Get users greenhouse photos from the server
+    function getUserPhotos () {
+      //get user photos urls from the server
+      //load different urls into src attributes
+      //for user images.
+    }
+
+    function removeUserPhoto () {
+      //Delete user photo from the vm photos url list.
+      //Send request to server to delete the specified
+      //photo url from the database
+    }
+
+    function addUserPhoto () {
+      //2 options
+      //Add a url to a photo which will get added to the
+      //src of an img tag and sent to the db for storage.
+
+      //or upload a photo, store the url in the database, 
+      //then fetch that url and ad it to the vm.photos array.
     }
 
 
