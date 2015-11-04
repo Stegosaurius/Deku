247443bcffe5a67d197d93a77694f19d5fbc69c0
Edgar Pabon <edgar.pabon@gmail.com>
2015-11-04 10:25:52 -0800

    Add status method returns the newly created status to client

--- server/controllers/statusController.js
+++ server/controllers/statusController.js
@@ -28,7 +28,8 @@ module.exports = {
             console.error(err);
             res.status(500).end();
           } else {
-            res.status(201).json(status);
+            console.log("status about to be sent is ", status)
+            res.status(201).json(status[0]);
           }
         });
       }
