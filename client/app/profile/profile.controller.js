5c9841b210c75837a70514d701bc29c0495dd92f
John Anderson <jfanderson373@gmail.com>
2015-11-06 14:38:20 -0800

    Reformat modals of user lists

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -167,12 +167,8 @@
     function getTags() {
       User.getTags(vm.username)
         .then(function(tags) {
-          if (tags.length === 0) {
-            vm.tags = ['Plants?', 'Methods/Technologies?', 'Interests?', 'Put them here.'];
-          } else {
-            for (var i = 0; i < tags.length; i++) {
-              vm.tags.push(tags[i].tag);
-            }
+          for (var i = 0; i < tags.length; i++) {
+            vm.tags.push(tags[i].tag);
           }
         });
     }
@@ -180,20 +176,22 @@
     function getFollowers() {
       User.getFollowers(vm.username)
         .then(function(data) {
-          for (var i = 0; i < data.length; i++) {
-            vm.followers.push(data[i].username);
-          }
+          vm.followers = data;
+
           // check whether active user is following this user
-          if (!vm.activeUser && (vm.followers.indexOf($window.localStorage.username) !== -1)) {
-            vm.isFollowing = true;
+          if (!vm.activeUser) {
+            for (var i = 0; i < vm.followers.length; i++) {
+              if (vm.followers[i].username === $window.localStorage.username) {
+                vm.isFollowing = true;
+                break;
+              }
+            }
           }
         });
 
       User.getFollowees(vm.username)
         .then(function(data) {
-          for (var i = 0; i < data.length; i++) {
-            vm.followees.push(data[i].username);
-          }
+          vm.followees = data;
         });
     }
 
