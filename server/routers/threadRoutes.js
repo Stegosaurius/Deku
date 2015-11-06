d11702edc3a64bc5c9b5630c9a259fed2098c9d5
Edgar Pabon <edgar.pabon@gmail.com>
2015-11-05 16:35:10 -0800

    Reordered thread routes

--- server/routers/threadRoutes.js
+++ server/routers/threadRoutes.js
@@ -7,11 +7,23 @@ module.exports = function (app) {
   // get all the messages for a page of a thread
   app.get('/messages/:userID/:threadID/:page', threadController.getMessagesByPage);
 
-  // Get recent messages posted to the forum for a user
-  app.get('/recent/:username', threadController.getRecentForumActivity);
+  // Delete message from a thread
+  app.delete('/messages/:messageID', threadController.deleteMessage);
 
   // get recent messages posted to forums by people the user is following
   app.get('/recent/followees/:userID', threadController.getRecentFolloweeActivity);
+  
+  // Get recent messages posted to the forum for a user
+  app.get('/recent/:username', threadController.getRecentForumActivity);
+
+   // Voting
+  app.post('/vote/message/:userID/:messageID/:threadID', threadController.likeMessage);
+
+  app.delete('/vote/message/:userID/:messageID/:threadID', threadController.unlikeMessage);
+  
+  app.post('/vote/:userID/:threadID', threadController.likeThread);
+
+  app.delete('/vote/:userID/:threadID', threadController.unlikeThread);
 
 
   // Create thread
@@ -26,16 +38,8 @@ module.exports = function (app) {
   //Route for posting a message to a particular thread
   app.post('/:userID/:threadID', threadController.addMessageToThread);
 
-  // Delete message from a thread
-  app.delete('/messages/:messageID', threadController.deleteMessage);
 
-  // Voting
-  app.post('/vote/message/:userID/:messageID/:threadID', threadController.likeMessage);
 
-  app.delete('/vote/message/:userID/:messageID/:threadID', threadController.unlikeMessage);
-  
-  app.post('/vote/:userID/:threadID', threadController.likeThread);
-
-  app.delete('/vote/:userID/:threadID', threadController.unlikeThread);
+ 
 
 }
